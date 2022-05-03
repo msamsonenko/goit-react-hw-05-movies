8967aa8164a7f7176fa85dba38a8be45b625c5f7
IrynaSam <52596926+msamsonenko@users.noreply.github.com>
2022-05-03 12:26:53 +0300

    lazy loading added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,31 @@
 //strict mode return
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './HomePage';
-import Navigation from './Navigation';
-import MoviesPage from './MoviesPage';
-import MovieDetailsPage from './MovieDetailsPage';
-import Cast from './Cast/Cast';
-import Reviews from './Reviews';
+
+const HomePage = lazy(() => import('./HomePage/HomePage'));
+const Navigation = lazy(() => import('./Navigation/Navigation'));
+const MoviesPage = lazy(() => import('./MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() =>
+  import('./MovieDetailsPage/MovieDetailsPage')
+);
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 export const App = () => {
   return (
     <div>
       <h1>Movie Finder</h1>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="movies/:movieId" element={<MovieDetailsPage />}>
-          <Route path="reviews" element={<Reviews />} />
-          <Route path="cast" element={<Cast />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="cast" element={<Cast />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
